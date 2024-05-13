65870babb37be5b7b5d2f47886f669300550c24b
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-05-13 14:37:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("######################");
+    System.out.println("# Banda: Linkin Park #");
+System.out.println("######################");
+    System.out.println("");
+    System.out.println("Album = Hybrid Theory");
+    System.out.println("Ano de Lançamento = 2009");
+    System.out.println("");
+    System.out.println("Hybrid Theory é o álbum de estreia da banda americana de rock Linkin Park. É o mais vendido do grupo até o momento, com cerca de mais de 30 milhões de cópias comercializadas pelo mundo, sendo disco de diamante nos Estados Unidos (vendendo mais de 12 milhões de cópias somente neste país). É o álbum de estreia mais vendido desde Appetite for Destruction (1987) de Guns N' Roses, o álbum de rock mais vendido do século XXI e um dos álbuns mais vendidos de todos os tempos.Também está na lista dos 200 álbuns definitivos no Rock and Roll Hall of Fame.");
+  }
+}
