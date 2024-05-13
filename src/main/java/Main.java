cdb8222781d8d11b7d35c39d3a5f3c85f2303f6d
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-05-13 14:58:21 +0000

    dia 13 de maio de 2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,21 @@ System.out.println("######################");
     System.out.println("Ano de Lançamento = 2009");
     System.out.println("");
     System.out.println("Hybrid Theory é o álbum de estreia da banda americana de rock Linkin Park. É o mais vendido do grupo até o momento, com cerca de mais de 30 milhões de cópias comercializadas pelo mundo, sendo disco de diamante nos Estados Unidos (vendendo mais de 12 milhões de cópias somente neste país). É o álbum de estreia mais vendido desde Appetite for Destruction (1987) de Guns N' Roses, o álbum de rock mais vendido do século XXI e um dos álbuns mais vendidos de todos os tempos.Também está na lista dos 200 álbuns definitivos no Rock and Roll Hall of Fame.");
+    System.out.println("");
+    System.out.println("Músicas do Albúm:");
+    System.out.println("");
+    System.out.println("Nome----Duração");
+    System.out.println("Papercut - 3:04");
+    System.out.println("One Step Closer - 2:35");
+    System.out.println("With You - 3:23");
+    System.out.println("Points of Authority - 3:20");
+    System.out.println("Crawling - 3:29");
+    System.out.println("Runaway - 3:03");
+    System.out.println("By Myself - 3:09");
+    System.out.println("In the End - 3:36");
+    System.out.println("A Place for My Head - 3:04");
+    System.out.println("Forgotten - 3:14");
+    System.out.println("Cure for the Itch - 2:37");
+    System.out.println("Pushing me Away - 3:11");
   }
 }
